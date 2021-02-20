f6876b7ee3e74f0c5ee04db745960ba6e07b8826
Vladimir Avakumov <avakdev@gmail.com>
2021-02-20 17:48:47 +0400

    new feature

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello2
+          Hello new feature
         </p>
         <a
           className="App-link"
